2be233e17cf8a75407e6b8c589219c4628ef6e47
fei Wang <wafei@wafei-00.local>
2021-07-19 21:38:36 -0400

    score test

--- bootstrap_robust/src/bootstraptest_ancova.py
+++ bootstrap_robust/src/bootstraptest_ancova.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from loguru import logger
-from scipy.stats import chi2
+from scipy.stats import norm, chi2, ttest_ind
 
 __version__ = '0.1'
 
@@ -20,13 +20,16 @@ class BootstrapAncova:
         self.x_centered = self.x - self.x.mean(axis=0)
         self.y_centered = self.y - self.y.mean(axis=0)
 
+        treatment_code = np.zeros((self.n, np.unique(treatment).shape[0]))
+        treatment_code[np.arange(self.n), self.treatment] = 1
+
         self.design = np.concatenate((np.ones((self.n, 1)),
-                                      self.treatment.reshape((-1, 1)),
-                                      self.x_centered),
+                                      self.x,
+                                      treatment_code[:, 1:]),
                                      axis=1)
 
         self.design_h0 = np.concatenate((np.ones((self.n, 1)),
-                                         self.x_centered),
+                                         self.x),
                                         axis=1)
 
         self.beta = np.zeros(self.design.shape[1])
@@ -51,99 +54,140 @@ class BootstrapAncova:
                 self.design.T.dot(self.inv_variance *
                                   self.y))
 
-            logger.info(self.beta)
-
             self.residual = self.y - self.design.dot(self.beta)
 
-            self.inv_variance[self.treatment == 0] = 1 / self.residual[
-                self.treatment
-                == 0].var()
-
-            self.inv_variance[self.treatment == 1] = 1 / self.residual[
-                self.treatment
-                == 1].var()
+            for j in np.unique(self.treatment):
+                self.inv_variance[self.treatment == j] = 1 / self.residual[
+                    self.treatment == j].var()
 
             i += 1
 
+        design_weight = self.design * self.inv_variance.reshape((-1, 1))
+        self.g = (design_weight).T.dot(self.y - self.design.dot(
+            self.beta)) / self.n
+        self.d_g = - design_weight.T.dot(self.design) / self.n
 
-        self.g = self.design.T.dot(self.y - self.design.dot(self.beta)) / self.n
+        self.v = design_weight.T.dot(self.design) / self.n
 
-        self.sn = self.design[self.treatment==1,].T.dot(self.design[
-                                                           self.treatment==1,
-                                                        ]) / self. n * \
-                  self.residual[self.treatment == 1].var() + self.design[self.treatment==0,].T.dot(self.design[
-                                                           self.treatment==0,
-                                                        ]) / self. n * \
-                  self.residual[self.treatment == 0].var()
-
-        self.xtx = self.design.T.dot(self.design) / self. n
         # variance of GMM estimator is
         # \sqrt n (\hat \beta - \beta_0) -> N(0, (G W G^T)^{-1})
         # G = \partial \bar g(beta)^T / \partial \beta
-        self.variance = np.linalg.inv(self.xtx.dot(np.linalg.inv(self.sn)).dot(
-            self.xtx)).diagonal() / self. n
 
+        self.w = np.linalg.inv(self.v)
+        self.variance = np.linalg.inv(self.d_g.dot(self.w).dot(
+            self.d_g.T)).diagonal() / self.n
 
-        self.gmm = self.g.dot(np.linalg.inv(self.sn)).dot(self.g) * self.n
+        self.gmm = self.g.dot(self.w).dot(self.g) * self.n
 
+        # under the H_0
         i = 0
         while i < self.maxiter:
-            self.beta_h0 = np.linalg.solve(self.design_h0.T.dot(
-                np.multiply(self.inv_variance.reshape((-1, 1)),
-                            self.design_h0)
+            beta_h0 = np.linalg.solve(self.design_h0.T.dot(
+                np.multiply(self.inv_variance.reshape((-1, 1)), self.design_h0)
             ),
                 self.design_h0.T.dot(self.inv_variance *
                                      self.y))
 
-            logger.info(self.beta_h0)
-
-            self.residual_h0 = self.y - self.design_h0.dot(self.beta_h0)
-
-            self.inv_variance_h0 = 1 / self.residual_h0.var()
             i += 1
 
-        self.g_h0 = self.design_h0.T.dot(self.y - self.design_h0.dot(
-            self.beta_h0)) / self.n
+        self.beta_h0 = np.zeros(self.design.shape[1])
+        # TODO repalce x.shape[1] + 1 to the number of covariates.
+        self.beta_h0[:(self.x.shape[1] + 1)] = beta_h0
 
-        self.sn_h0 = self.design_h0[self.treatment==1,].T.dot(self.design_h0[
-                                                           self.treatment==1,
-                                                        ]) / self. n * \
-                  self.residual[self.treatment == 1].var() + self.design_h0[
-                         self.treatment==0,].T.dot(self.design_h0[
-                                                           self.treatment==0,
-                                                        ]) / self. n * \
-                  self.residual[self.treatment == 0].var()
+        self.g_h0 = design_weight.T.dot(
+            self.y - self.design.dot(self.beta_h0)) / \
+                    self.n
+
+        self.v_h0 = design_weight * (self.y - self.design.dot(
+            self.beta_h0)).reshape((-1, 1))
+        self.v_h0 = self.v_h0.T.dot(self.v_h0) / self.n
 
 
-        self.gmm_h0 = self.n * self.g_h0.dot(np.linalg.inv(
-            self.sn_h0)).dot(self.g_h0)
+        self.d_g_h0 = -design_weight.T.dot(self.design) / self.n
+
+        self.gmm_h0 = self.n * self.g_h0.dot(self.w).dot(self.g_h0)
+
+        self.d_gmm_h0 = self.d_g_h0.dot(self.w).dot(self.g_h0)
+
+        self.gwg = self.d_g_h0.dot(self.w.dot(self.v_h0).dot(self.w)).dot(
+            self.d_g_h0.T)
+
+        self.score = self.d_gmm_h0.T.dot(np.linalg.inv(
+            self.gwg)).dot(self.d_gmm_h0) * self.n
 
         self.lr = self.gmm_h0 - self.gmm
 
-        logger.info(self.lr)
+        logger.info(f"lr={self.lr}, score={self.score}")
+
+    def cuped(self):
+        self.theta = np.linalg.solve(self.design_h0[:, [0, 3]].T.dot(
+            self.design_h0[:, [0, 3]]),
+            self.design_h0[:, [0, 3]].T.dot(self.y))
+
+        self.y_cv = self.y - self.design_h0[:, [0, 3]].dot(self.theta)
 
 
 if __name__ == '__main__':
     nrep = 5000
-    beta = np.zeros((nrep, 6))
-    lr = np.zeros(nrep)
-    variance = np.zeros((nrep, 6))
-    n = 400
-    for i in range(nrep):
-        x = np.random.normal(0, 1, 4 * n).reshape((n, -1))
-        treatment = np.random.binomial(1, 0.5, n)
-        y = 1 + treatment * 0 + x.dot([1,2,3,4]) + np.random.normal(0, 0.5, n)
 
-        model = BootstrapAncova(x, y, treatment, 5)
+    beta_cv = np.zeros((nrep, 1))
+    lr = np.zeros(nrep)
+    score = np.zeros(nrep)
+    variance = np.zeros((nrep, 5))
+    n = 400
+    np.random.seed(10)
+    pv = np.zeros(nrep)
+    pv_cv = np.zeros(nrep)
+
+    n_treat = 3
+    effect = [0, 0.0, 0.3]
+    var = [0.2, 0.2, 0.2]
+    beta = np.zeros((nrep, 3 + n_treat))
+
+    variance = np.zeros((nrep, 3 + n_treat))
+
+    for i in range(nrep):
+        x = np.random.normal(10, 1, 4 * n).reshape((n, -1))
+        treatment = np.random.uniform(0, 2, n).round().astype(int)
+
+        y = np.zeros(n)
+        for j in range(3):
+            epsilon = np.random.normal(0, var[j], np.sum(treatment == j))
+            epsilon = epsilon - epsilon.mean()
+
+            y[treatment == j] = 1 + effect[j] + x[treatment == j,].dot([1, 2, 3,
+                                                                        4]) + epsilon
+
+        logger.info(y.var())
+
+        model = BootstrapAncova(x[:, :3], y, treatment, 5)
         model.fit()
+        model.cuped()
+        y_cv = model.y_cv
         beta[i,] = model.beta
         lr[i] = model.lr
+        score[i] = model.score
         variance[i,] = model.variance
-
+        pv[i] = ttest_ind(y[treatment == 0], y[treatment == 2],
+                          equal_var=False)[1]
+        pv_cv[i] = ttest_ind(y_cv[treatment == 0], y_cv[treatment == 2],
+                             equal_var=False)[1]
+        beta_cv[i] = y_cv[treatment == 2].mean() - y_cv[treatment == 0].mean()
     plt.figure()
-    plt.hist(beta[:, 1], bins=50)
-    plt.axvline(0.1)
-    plt.show()
+    fig, axs = plt.subplots(5)
+    axs[0].hist(beta[:, -1], bins=50, alpha=0.2)
+    axs[0].hist(beta_cv, bins=50, alpha=0.5)
 
-    plt.hist(lr, bins=50)
+    z = (beta[:, 1] - 0) / variance[:, 1] ** 0.5
+
+    axs[1].hist((1 - norm.cdf(abs(z))) * 2, bins=50, alpha=0.5)
+
+    axs[2].hist((1 - chi2.cdf(score, df=2)), bins=50, alpha=0.5)
+    axs[3].hist(pv, bins=50, alpha=0.5)
+    axs[4].hist(pv_cv, bins=50, alpha=0.5)
+
     plt.show()
+    logger.info(f"pv={np.sum(pv < 0.05) / nrep}, pv_cv="
+                f"{np.sum(pv_cv < 0.05) / nrep}, score="
+                f"{np.sum(score > chi2.ppf(0.95, df=2)) / nrep}, lr"
+                f"={np.sum(lr > chi2.ppf(0.95, df=2)) / nrep}")
